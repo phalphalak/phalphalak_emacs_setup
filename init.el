5444a0fe934212ae4d4ce663204f827b485c5363
phalphalak@gmail.com <phalphalak@gmail.com>
2012-07-13 00:09:03 +0200

    add solarized via emacs24-theme functionality + adjust indentation

--- init.el
+++ init.el
@@ -2,6 +2,17 @@
 (add-to-list 'load-path "~/.emacs.d/addons/paredit")
 (add-to-list 'load-path "~/.emacs.d/addons/highlight-parentheses")
 (add-to-list 'load-path "~/.emacs.d/addons/auto-complete")
+;;(add-to-list 'load-path "~/.emacs.d/addons/color-theme-6.6.0")
+(add-to-list 'load-path "~/.emacs.d/addons/emacs-color-theme-solarized")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+;;(require 'color-theme)
+;;(eval-after-load "color-theme"
+;;  '(progn
+;;     (color-theme-initialize)
+;;     (color-theme-hober)))
+
+(load-theme 'solarized-dark t)
+;;(color-theme-gnome2)
 
 (require 'paredit)
 (require 'clojure-mode)
@@ -22,5 +33,20 @@
 ;; require or autoload paredit-mode
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
+(add-hook 'slime-repl-mode-hook 'turn-on-paredit)
 (add-hook 'clojure-mode-hook (lambda () (highlight-parentheses-mode +1)))
+(add-hook 'slime-repl-mode-hook (lambda () (highlight-parentheses-mode +1)))
 
+(global-linum-mode t) ;;show line numbers
+
+(global-hl-line-mode t) ;; highlight current line
+
+;;; Prevent Extraneous Tabs
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+;;(setq standard-indent 2)
+(setq tab-stop-list '(2 4 6 8 12 14 16 18))
+;;(setq tabbar-background-color "black")
+
+(setq highlight-tabs t)
+(setq highlight-trailing-whitespace t)
--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
-wget "http://mumble.net/~campbell/emacs/paredit.el" -O addons/paredit/paredit.el
-wget "http://nschum.de/src/emacs/highlight-parentheses/highlight-parentheses.el" -O addons/highlight-parentheses/highlight-parentheses.el
+#wget "http://mumble.net/~campbell/emacs/paredit.el" -O addons/paredit/paredit.el
+#wget "http://nschum.de/src/emacs/highlight-parentheses/highlight-parentheses.el" -O addons/highlight-parentheses/highlight-parentheses.el
+#wget "http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz"
+#tar -xzf color-theme-6.6.0.tar.gz
+#rm color-theme-6.6.0.tar.gz
