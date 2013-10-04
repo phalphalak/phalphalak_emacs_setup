db963480eeab48695fa2d6012e04de3d45fe6c83
Roman Flammer <roman@Romans-MacBook-Pro.local>
2013-10-04 13:34:21 +0200

    add rainbow edit.

--- init.el
+++ init.el
@@ -1,10 +1,9 @@
 (setq backup-directory-alist `((".*" . ,"~/emacs-temp")))
 (setq auto-save-file-name-transforms `((".*" ,"~/emacs-temp" t)))
 
-(add-to-list 'load-path "~/.emacs.d/addons/nrepl")
+(tool-bar-mode -1)
+
 (add-to-list 'load-path "~/.emacs.d/addons/clojure-mode")
-(add-to-list 'load-path "~/.emacs.d/addons/paredit")
-(add-to-list 'load-path "~/.emacs.d/addons/highlight-parentheses")
 (add-to-list 'load-path "~/.emacs.d/addons/auto-complete")
 (add-to-list 'load-path "~/.emacs.d/addons/smooth-scroll")
 (add-to-list 'load-path "~/.emacs.d/addons/org-7.9.3d")
@@ -20,10 +19,26 @@
 (load-theme 'solarized-dark t)
 ;;(color-theme-gnome2)
 
+;; ---------------------------------------------------------------------------------
+;; nrepl
+(add-to-list 'load-path "~/.emacs.d/addons/nrepl")
 (require 'nrepl)
+;; ---------------------------------------------------------------------------------
+;; highlieght parenthesis
+;(add-to-list 'load-path "~/.emacs.d/addons/highlight-parentheses")
+;(require 'highlight-parentheses)
+;(add-hook 'clojure-mode-hook (lambda () (highlight-parentheses-mode +1)))
+;(add-hook 'slime-repl-mode-hook (lambda () (highlight-parentheses-mode +1)))
+;; ---------------------------------------------------------------------------------
+;; rainbow delimiters
+(add-to-list 'load-path "~/.emacs.d/addons/rainbow-delimiters")
+(require 'rainbow-delimiters)
+(global-rainbow-delimiters-mode)
+;; ---------------------------------------------------------------------------------
+(add-to-list 'load-path "~/.emacs.d/addons/paredit")
 (require 'paredit)
+;; ---------------------------------------------------------------------------------
 (require 'clojure-mode)
-(require 'highlight-parentheses)
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/addons/auto-complete/ac-dict")
 (ac-config-default)
@@ -45,8 +60,6 @@
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
 (add-hook 'slime-repl-mode-hook 'turn-on-paredit)
-(add-hook 'clojure-mode-hook (lambda () (highlight-parentheses-mode +1)))
-(add-hook 'slime-repl-mode-hook (lambda () (highlight-parentheses-mode +1)))
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (global-linum-mode t) ;;show line numbers
@@ -131,3 +144,73 @@
  )
 
 
+
+(defun unicode-symbol (name)
+  "Translate a symbolic name for a Unicode character -- e.g., LEFT-ARROW
+  or GREATER-THAN into an actual Unicode character code. "
+  (decode-char 'ucs (case name
+                        ;; arrows
+                        ('left-arrow 8592)
+                        ('up-arrow 8593)
+                        ('right-arrow 8594)
+                        ('down-arrow 8595)
+                        ;; boxes
+                        ('double-vertical-bar #X2551)
+                        ;; relational operators
+                        ('equal #X003d)
+                        ('not-equal #X2260)
+                        ('identical #X2261)
+                        ('not-identical #X2262)
+                        ('less-than #X003c)
+                        ('greater-than #X003e)
+                        ('less-than-or-equal-to #X2264)
+                        ('greater-than-or-equal-to #X2265)
+                        ;; logical operators
+                        ('logical-and #X2227)
+                        ('logical-or #X2228)
+                        ('logical-neg #X00AC)
+                        ;; misc
+                        ('nil #X2205)
+                        ('horizontal-ellipsis #X2026)
+                        ('double-exclamation #X203C)
+                        ('prime #X2032)
+                        ('double-prime #X2033)
+                        ('for-all #X2200)
+                        ('there-exists #X2203)
+                        ('element-of #X2208)
+                        ;; mathematical operators
+                        ('square-root #X221A)
+                        ('squared #X00B2)
+                        ('cubed #X00B3)
+                        ;; letters
+                        ('lambda #X03BB)
+                        ('alpha #X03B1)
+                        ('beta #X03B2)
+                        ('gamma #X03B3)
+                        ('delta #X03B4))))
+
+(defun substitute-pattern-with-unicode (pattern symbol)
+  "Add a font lock hook to replace the matched part of PATTERN with the
+  Unicode symbol SYMBOL looked up with UNICODE-SYMBOL."
+  (interactive)
+  (font-lock-add-keywords
+   nil `((,pattern (0 (progn (compose-region (match-beginning 1) (match-end 1)
+                                             ,(unicode-symbol symbol))
+                             nil))))))
+
+(defun substitute-patterns-with-unicode (patterns)
+  "Call SUBSTITUTE-PATTERN-WITH-UNICODE repeatedly."
+  (mapcar #'(lambda (x)
+              (substitute-pattern-with-unicode (car x)
+                                               (cdr x)))
+          patterns))
+
+(defun clojure-unicode ()
+  (interactive)
+  (substitute-patterns-with-unicode
+   (list (cons "\\(->\\)" 'right-arrow)
+         (cons "\\(and\\)" 'logical-and)
+         (cons "\\(fn\\)" 'lambda)
+   )))
+
+;(add-hook 'clojure-mode-hook 'clojure-unicode)
